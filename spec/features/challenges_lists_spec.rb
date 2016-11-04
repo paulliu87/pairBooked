4bedafbf77f9353613a9f4c045a3d617d72eddef
Jeffrey Mendiola & Paul Liu <pair+jwmendio.paulliu87@devbootcamp.com>
2016-11-04 11:50:44 -0700

    Create the test and erb file for students can see challenges list

---
+++ spec/features/challenges_lists_spec.rb
@@ -0,0 +1,14 @@
+feature "students can see challenges list" do
+  context "when students visit the challenge index page" do
+    let!(:challenge1) { Challenge.create(name: "2.2 Links, Images, and Layouts", due_date: DateTime.new(2016,10,12,12,00,00)) }
+    let(:challenge2) { Challenge.create(name: "2.4 Forms", due_date: DateTime.new(2016,10,15,12,00,00)) }
+    let(:challenge3) { Challenge.create(name: "3.2 Link Tag", due_date: DateTime.new(2016,10,20,12,00,00)); }
+
+    xscenario "students can click on a challenge" do
+      visit '/challenges'
+      # p find_by_class('.challenge')
+      click_link '2.2 Links, Images, and Layouts'
+      expect(page).to redirect_to("/challenges/#{challenge1.id}/timeslots")
+    end
+  end
+end
