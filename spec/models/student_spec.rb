2b4e654bc7062f6f95685b751e1b4232719267f3
Sunjay Kumar <lorenzianheteroceran@gmail.com>
2016-11-04 09:59:06 -0700

    Wrote student specs

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,5 +1,39 @@
 require 'rails_helper'
 
 RSpec.describe Student, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let(:student) {Student.new(
+    username: "sleepyhead",
+    fullname: "Rip van Winkle",
+    email: "sleepyheadzzz@gmail.com",
+    slack_name: "rvwinkle"
+    )}
+
+  describe "validations" do
+    it "saves with valid information" do
+      expect(student.save).to be true
+    end
+
+    it "saves without a slackname" do
+      student.slack_name = nil
+      expect(student.save).to be true
+    end
+
+    it "does not allow without username" do
+      student.username = nil
+      expect(student.save).to be false
+    end
+
+    it "does not allow without email" do
+      student.email = nil
+      expect(student.save).to be false
+    end
+  end
+
+  pending "associations" do
+    let(:timeslot) {
+
+    }
+    it 'has a timeslot'
+  end
 end
