865184d3f4fe1bda2906e73781e1a5521d08f021
Sunjay Kumar <lorenzianheteroceran@gmail.com>
2016-11-04 10:25:26 -0700

    Login now authentication for namespace reasons, routes set up and tested

---
+++ app/controllers/authentication_controller.rb
@@ -0,0 +1,19 @@
+class AuthenticationController < ApplicationController
+  def index
+  end
+
+  def login
+    auth = request.env["omniauth.auth"]
+    student = Student.find_by_provider_and_uid(
+      auth["provider"],
+      auth["uid"]
+    ) || Student.create_with_omniauth(auth)
+    session[:student_id] = student.id
+    redirect_to root_url, :notice => "Signed in!"
+  end
+
+  def logout
+    session[:student_id] = nil
+    redirect_to root_url, :notice => "Signed out!"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'login#index'
+  get '/logout', to: "authentication#logout"
+  post '/login', to: "authentication#login"
+
+  root to: 'authentication#index'
 end
