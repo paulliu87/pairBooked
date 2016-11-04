656753783ad064f20f9791c9800f135e95cd6656
Sunjay Kumar <lorenzianheteroceran@gmail.com>
2016-11-04 12:04:12 -0700

    Fixed scoping for minimal access

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer unless Rails.env.production?
   p "Hello"
-  provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"], scope: "user"
+  provider :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"], scope: "user:email"
 end
