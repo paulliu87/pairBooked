c7a993c576cbd548cf3959380b3362827c0bafe1
Sunjay Kumar <lorenzianheteroceran@gmail.com>
2016-11-04 17:43:53 -0700

    View tested and worked

---
+++ spec/views/timeslots/index_html_erb_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe "challenges/1/timeslots/index.html.erb", type: :view do
+  describe "students can see a list of timeslots" do
+    let!(:timeslot1) { FactoryGirl.create(:timeslot) }
+    let!(:timeslot2) { FactoryGirl.create(:timeslot) }
+    let!(:timeslot3) { FactoryGirl.create(:timeslot) }
+
+    before(:each) do
+      @timeslots = Timeslot.all
+      @challenge = timeslot1.challenge
+      render :template => "/timeslots/index.html.erb"
+    end
+
+    it "displays list" do
+      rendered.should match(/\+ Create/)
+      p @timeslots
+      rendered.should match(Regexp.new timeslot1.start_at.to_s)
+      rendered.should match(Regexp.new timeslot2.start_at.to_s)
+    end
+  end
+end
