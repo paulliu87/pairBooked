80792f969f1094f72202cf6241750c419e53517b
Sunjay Kumar <lorenzianheteroceran@gmail.com>
2016-11-04 20:03:22 -0700

    Made timeslots less predictable

--- spec/factories/timeslots.rb
+++ spec/factories/timeslots.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :timeslot do
     initiator_id {FactoryGirl.create(:student).id}
-    start_at {Time.now + rand(7).floor.days}
+    start_at {Time.now + rand(168).floor.hours}
     end_at {start_at + 4.days + 2.hours}
     challenge_id {FactoryGirl.create(:challenge).id}
 
