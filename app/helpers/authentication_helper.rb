d96d40d0198c120c6fe3e3344f21663435a5403b
Sunjay Kumar <lorenzianheteroceran@gmail.com>
2016-11-07 15:51:50 -0800

    IDK man

--- app/helpers/authentication_helper.rb
+++ app/helpers/authentication_helper.rb
@@ -3,5 +3,6 @@ module AuthenticationHelper
     timezone_str = Student.find(session[:student_id]).time_zone
     newzone = ActiveSupport::TimeZone.new(timezone_str)
     oldzone = date.zone
+
   end
 end
