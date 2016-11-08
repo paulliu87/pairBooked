03eda6d7a83408ef73afe4628d41c860803714d7
Sunjay Kumar <lorenzianheteroceran@gmail.com>
2016-11-07 19:00:48 -0800

    IDK

--- spec/controllers/authentication_controller_spec.rb
+++ spec/controllers/authentication_controller_spec.rb
@@ -58,4 +58,33 @@ RSpec.describe AuthenticationController, type: :controller do
       expect(student.time_zone).to eq("Eastern Time (US & Canada)")
     end
   end
+
+  describe "#dashboard" do
+    let(:acceptor) { Student.find(session[:student_id]) }
+    let(:initiator) { FactoryGirl.create(:student) }
+    let(:paired_timeslot) {
+      timeslot = FactoryGirl.create(:timeslot)
+      timeslot.update_attributes(acceptor: acceptor, initiator: initiator)
+      timeslot
+    }
+    let(:unpaired_timeslot) {
+      timeslot = FactoryGirl.create(:timeslot)
+      timeslot.update_attributes(acceptor: nil, initiator: Student.find(session[:student_id]))
+      timeslot
+    }
+
+    before do
+      @request.session[:student_id] = FactoryGirl.create(:student).id
+    end
+
+    it "knows which timeslots you're paired in" do
+      paired_timeslot
+      expect(assigns(:paired_timeslots)).to include(paired_timeslot)
+    end
+
+    it "knows your empty timeslots" do
+      unpaired_timeslot
+      expect(assigns(:unpaired_timeslots)).to include(unpaired_timeslot)
+    end
+  end
 end
