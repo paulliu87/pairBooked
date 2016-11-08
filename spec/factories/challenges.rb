24ef9cf981be77910a6c6f4f08276173983a5f37
Sunjay Kumar <lorenzianheteroceran@gmail.com>
2016-11-08 15:23:49 -0800

    Seeds fixed

--- spec/factories/challenges.rb
+++ spec/factories/challenges.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :challenge do
     name {"1.3 Ruby"}
-    due_date {Time.now + 7.days}
+    due_date {Time.now.beginning_of_hour + 7.days}
   end
 end
--- spec/factories/timeslots.rb
+++ spec/factories/timeslots.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :timeslot do
     initiator_id {FactoryGirl.create(:student).id}
-    start_at {Time.now + 4.days + rand(168).floor.hours}
-    end_at {start_at + 2.hours}
+    start_at {Time.now.beginning_of_hour + 4.days + rand(168).floor.hours}
+    end_at {start_at + 1.hours}
     challenge_id {FactoryGirl.create(:challenge).id}
 
     trait :no_initiator_id do
@@ -14,7 +14,7 @@ FactoryGirl.define do
     end
 
     trait :old_start_at_date do
-      start_at DateTime.now.days_ago(1)
+      start_at DateTime.now.days_ago(1).beginning_of_hour
     end
 
     trait :standard do
