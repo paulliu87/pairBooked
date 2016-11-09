1a1b4da4f2ebfd00b25d1e89e2ae2fd8d4f167e7
Jeffrey Mendiola & Paul Liu <pair+jwmendio.paulliu87@devbootcamp.com>
2016-11-09 09:12:39 -0800

    Commit before the merge

--- spec/views/timeslots/new_html_erb_spec.rb
+++ spec/views/timeslots/new_html_erb_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "challenges/1/timeslots/new.html.erb", type: :view do
-  xdescribe "students can see a form that creates a new page" do
+  describe "students can see a form that creates a new page" do
     let!(:challenge) { FactoryGirl.create(:challenge) }
 
     before(:each) do
@@ -9,8 +9,10 @@ RSpec.describe "challenges/1/timeslots/new.html.erb", type: :view do
         stub_model(Challenge,
           name: "2.2 Links, Images, and Layouts",
           due_date: DateTime.new(2016,10,12,12,00,00)
-        ),
+        )
       ])
+
+      params[:challenge_id] = challenge.id
       render :template => "timeslots/new.html.erb"
     end
 
@@ -18,6 +20,10 @@ RSpec.describe "challenges/1/timeslots/new.html.erb", type: :view do
       rendered.should contain('Start date')
       rendered.should contain('Start time')
       rendered.should contain('End time')
+    end
+
+    it "displays dropdown menu" do
+      expect(page).not_to have_select("drop_down_id", options: item3.name)
     end
   end
 end
