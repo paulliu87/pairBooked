17765da5250f79f72aaa94911f48fd139a4c81fb
Sunjay Kumar <lorenzianheteroceran@gmail.com>
2016-11-10 12:00:48 -0800

    Click to change reveals form

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -1,3 +1,12 @@
 $(document).ready( function(){
-
+  // Slack username
+  $("#slack-edit-button").on("click", function(e){
+    e.preventDefault();
+    $("#slack-edit-button").addClass("hidden");
+    $("form#update-slack").removeClass("hidden");
+  });
+  $("form#update-slack").on('submit', function(e) {
+    $("#slack-edit-button").removeClass("hidden");
+    $("form#update-slack").addClass("hidden");
+  });
 });
