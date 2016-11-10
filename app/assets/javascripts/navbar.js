40869df8dbe19ed0b9c041f594d4f0e7db662004
Sunjay Kumar <lorenzianheteroceran@gmail.com>
2016-11-10 12:10:55 -0800

    Errors are red

--- app/assets/javascripts/navbar.js
+++ app/assets/javascripts/navbar.js
@@ -5,8 +5,14 @@ $(document).ready( function(){
     $("#slack-edit-button").addClass("hidden");
     $("form#update-slack").removeClass("hidden");
   });
+
+  // Clicking the change button should:
+  //  Flip the edit button back to visible and hide the form
+  //  Submit request to server to update slack
+  //  Update the sidebar with the new slack name
   $("form#update-slack").on('submit', function(e) {
     $("#slack-edit-button").removeClass("hidden");
     $("form#update-slack").addClass("hidden");
+
   });
 });
