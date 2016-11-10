a7afd158644582bf8fb694230e17301d5c649fbc
Jeffrey Mendiola & Paul Liu <pair+jwmendio.paulliu87@devbootcamp.com>
2016-11-10 10:48:52 -0800

    Add more feature test

--- spec/features/challenges_lists_spec.rb
+++ spec/features/challenges_lists_spec.rb
@@ -4,12 +4,12 @@ RSpec.feature "students can see challenges list" do
     let!(:challenge1) { Challenge.create(name: "2.2 Links, Images, and Layouts", due_date: DateTime.new(2016,10,12,12,00,00)) }
     let(:challenge2) { Challenge.create(name: "2.4 Forms", due_date: DateTime.new(2016,10,15,12,00,00)) }
     let(:challenge3) { Challenge.create(name: "3.2 Link Tag", due_date: DateTime.new(2016,10,20,12,00,00)); }
-
+    given!(:student) { FactoryGirl.create(:student) }
     scenario "students can click on a challenge" do
+      page.set_rack_session(student_id: student.id)
       visit '/challenges'
-
-      click_link '2.2 Links, Images, and Layouts'
-      expect(page).to redirect_to("/challenges/#{challenge1.id}/timeslots")
+      click_link("2.2 Links, Images, and Layouts")
+      expect(page.current_path).to eq("/challenges/#{challenge1.id}/timeslots")
     end
   end
 end
