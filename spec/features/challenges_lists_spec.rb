3783a8deddc5875b70414a1cad89f42543f13c58
Jeffrey Mendiola & Paul Liu <pair+jwmendio.paulliu87@devbootcamp.com>
2016-11-10 09:45:34 -0800

    Merge commit

--- spec/features/challenges_lists_spec.rb
+++ spec/features/challenges_lists_spec.rb
@@ -5,9 +5,9 @@ RSpec.feature "students can see challenges list" do
     let(:challenge2) { Challenge.create(name: "2.4 Forms", due_date: DateTime.new(2016,10,15,12,00,00)) }
     let(:challenge3) { Challenge.create(name: "3.2 Link Tag", due_date: DateTime.new(2016,10,20,12,00,00)); }
 
-    xscenario "students can click on a challenge" do
+    scenario "students can click on a challenge" do
       visit '/challenges'
-      # p find_by_class('.challenge')
+
       click_link '2.2 Links, Images, and Layouts'
       expect(page).to redirect_to("/challenges/#{challenge1.id}/timeslots")
     end
